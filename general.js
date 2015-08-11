90a690fe3d38cae0ec3d1907ddc2fc3126c42877
Richard Keller <richard.be.jamin@gmail.com>
2015-08-11 14:26:36 -0500

    Update general.js

--- general.js
+++ general.js
@@ -1,7 +1,26 @@
 function rbk_toggle_class( el, el_class ){
-		if( el.className.match( el_class ) ){
-			el.className = el.className.replace(el_class, '');
-		} else {
-			el.className = el.className + ' ' + el_class;
-		}
+	if( el.className.match( el_class ) ){
+		el.className = el.className.replace(el_class, '');
+	} else {
+		el.className = el.className + ' ' + el_class;
 	}
+}
+function inArray( value, array ){
+	if( array.length > 0 ) {
+	    for( var i=0; i < array.length; i++ ){
+	        if( String(array[i]).match( value ) ){
+	            return true;
+	        }
+	    }
+	} else {
+	    return false;
+	}
+}
+function removeFalseValues( array ){
+	for(var i=0; i < array.length; i++){
+	    if( !array[i] || array[i] == false || array[i] == 'undefined'){
+	        array.splice(i);
+	    }
+	}
+	return array;
+}
